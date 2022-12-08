4431ce3873a8114178e04cdce199d527417efced
daviatkea <54393843+daviatkea@users.noreply.github.com>
2022-12-08 08:20:12 +0100

    New start

--- js/index.js
+++ js/index.js
@@ -1,25 +1,13 @@
 const vidContainer = document.querySelector(".video-player");
 const vid = document.querySelector(".video-player video");
 const btn = document.querySelector(".video-player-button");
-vid.playbackRate = 6; // Debug only
 
 function togglePlay() {
-    if (vid.paused) {
-        vid.play()
-        vidContainer.classList.add("playing")
-        btn.textContent = "Pause"
-    } else {
-        vid.pause()
-        btn.textContent = "Play"
-        vidContainer.classList.remove("playing")
-    }
+    
 }
 
 function videoEnded() {
-    if (vidContainer.classList.contains("playing")) {
-        vidContainer.classList.remove("playing")
-    }
-    btn.textContent = "Play"
+    
 }
 
 btn.addEventListener("click", togglePlay);
